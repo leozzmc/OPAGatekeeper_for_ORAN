7bed7cb7d041c65de08ee0991d140044b2ed28e6
leozzmc <30616512+leozzmc@users.noreply.github.com>
2022-03-24 14:57:21 +0800

    Add files via upload

--- Testing/Deploy_NearRTRIC.sh
+++ Testing/Deploy_NearRTRIC.sh
@@ -6,38 +6,33 @@
 
 # U Should clone the repo first!
 
-ROOTDIR = /root
+OPAROOTDIR=/root
 
 sudo apt-get update
 sudo apt-get install -y vim git curl net-tools
 
-cd ROOTDIR
-git clone https://gerrit.o-ran-sc.org/r/dep
+cd $OPAROOTDIR
+git clone https://gerrit.o-ran-sc.org/r/it/dep
 cd dep && git submodule update --init --recursive --remote
 
 ### Setup K8S
 echo "Setting the Kubernetes Node ..........."
 sleep 5
 
-cd ROOTDIR
-cd OPAGatekeeper_for_ORAN/
+cd $OPAROOTDIR/OPAGatekeeper_for_ORAN-main/
+
 sudo chmod +x ./k8s-1node-cloud-init-k_1_21-h_3_8-d_cur.sh
 mv -f ./k8s-1node-cloud-init-k_1_21-h_3_8-d_cur.sh  /root/dep/tools/k8s/bin/
 cd /root/dep/tools/k8s/bin/
 ./k8s-1node-cloud-init-k_1_21-h_3_8-d_cur.sh
 
-echo "Set up environment successfully. Wait for Pods Running"
-sleep 10
-while [[$(kubectl get pod -n kube-system | wc -1) -ne 9]]  # It should have 9 entries
-do
-  echo "Wait for Pod to run."
-  sleep 5
-done
+echo "Set up environment successfully. Wait for 5 Seconds"
+sleep 5
 
 ### Setup NearRT RIC Environment
-cd ~/dep/bin
+cd /root/dep/bin
 
-./deploy-ric-platform -f ../RECIPE_EXAMPLE/PLATFORM/example_recipe_oran_e_release.yaml
+./deploy-ric-platform -f ~/dep/RECIPE_EXAMPLE/PLATFORM/example_recipe_oran_e_release.yaml
 
 ###### It would take an hour to settle it down completely
 
@@ -46,22 +41,8 @@ kubectl get pod -n ricplt
 kubectl get pod -n ricplt  | wc -l
 
 echo "Near-RT RIC Installed Successful!"
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 echo "[■] Kubernetes Version: v1.21.1"
 echo "[■] Helm Version: v3.8.0"
+echo "Please enter \"kubectl get pod -n ricplt\" to ensure every pod is in \"Running\" state"
 sleep 5
 
